bbca6bf94437b00ae665e0a9d592e53286b8e88d
Natalia <naty-aragao@hotmail.com>
2023-10-27 00:36:15 -0300

    tentativa em vão de calcular as 3 metricas

--- naive_bayes.py
+++ naive_bayes.py
@@ -23,37 +23,61 @@ y = data[:,-1]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-def gaussiano(valor, media, desvio):
-    return np.exp(-np.power(valor - media, 2.) / (2 * np.power(desvio, 2.)))
-
-def probabilidade(y, z):
+def probabilidade_gaussiana(y, z):
     prob = 1
     for j in np.arange(0, z.shape[1]):
         media = np.mean(z[:, j])
         desvio = np.std(z[:, j])
-        prob = prob * gaussiano(y[j], media, desvio)
+        prob = prob * np.exp(-np.power(y[j] - media, 2.) / (2 * np.power(desvio, 2.)))
     return prob
 
-P = pd.DataFrame(data=np.zeros((X_test.shape[0], len(classes))), columns=classes)
+def probabilidade_multinomial(y, z):
+    prob = 1
+    for j in np.arange(0, z.shape[1]):
+        count_y = (z[:, j] == y[j]).sum()
+        prob = prob * count_y / len(z)
+    return prob
+
+def probabilidade_bernoulli(y, z):
+    prob = 1
+    for j in np.arange(0, z.shape[1]):
+        prob = prob * (z[:, j] == y[j]).mean()
+    return prob
+
+P_gaussiano = pd.DataFrame(data=np.zeros((X_test.shape[0], len(classes))), columns=classes)
+P_multinomial = pd.DataFrame(data=np.zeros((X_test.shape[0], len(classes))), columns=classes)
+P_bernoulli = pd.DataFrame(data=np.zeros((X_test.shape[0], len(classes))), columns=classes)
+
 for i in np.arange(0, len(classes)):
-    elementos = tuple(np.where(y_train==float(classes[i])))
+    elementos = tuple(np.nonzero(y_train==float(classes[i])))
     z = X_train[elementos,:][0]
     for j in np.arange(0, X_test.shape[0]):
         x = X_test[j,:]
-        pj = probabilidade(x, z)
-        P[classes[i]][j] = pj*len(elementos)/X_train.shape[0]
+        pj_gaussiana = probabilidade_gaussiana(x, z)
+        P_gaussiano[classes[i]][j] = (pj_gaussiana * len(elementos[0])) / X_train.shape[0]
 
-y_pred = []
-for i in np.arange(0, P.shape[0]):
-    c = np.argmax(np.array(P.iloc[[i]]))
-    y_pred.append(P.columns[c])
-y_pred = np.array(y_pred, dtype=str)
+        pj_multinomial = probabilidade_multinomial(x, z)
+        P_multinomial[classes[i]][j] = (pj_multinomial * len(elementos[0])) / X_train.shape[0]
 
-y_test_string = []
-for i, valor in enumerate(y_test):
-    valor = str(valor)
-    y_test_string.append(valor.rstrip('0').rstrip('.'))
-y_test_string = np.array(y_test_string, dtype=str) 
+        pj_bernoulli = probabilidade_bernoulli(x, z)
+        P_bernoulli[classes[i]][j] = (pj_bernoulli * len(elementos[0])) / X_train.shape[0]
 
-score = accuracy_score(y_pred, y_test_string)
-print("Acurácia: ", score)
+
+distance_metrics = ['gaussiana', 'multinomial', 'bernoulli'] 
+index = 0
+for matriz in [P_gaussiano, P_multinomial, P_bernoulli]:
+    y_pred = []
+    for i in np.arange(0, matriz.shape[0]):
+        c = np.argmax(np.array(matriz.iloc[[i]]))
+        y_pred.append(matriz.columns[c])
+    y_pred = np.array(y_pred, dtype=str)
+
+    y_test_string = []
+    for i, valor in enumerate(y_test):
+        valor = str(valor)
+        y_test_string.append(valor.rstrip('0').rstrip('.'))
+    y_test_string = np.array(y_test_string, dtype=str) 
+
+    score = accuracy_score(y_pred, y_test_string)
+    print(f"Métrica = {distance_metrics[index]}, Acurácia = {score:.2f}")
+    index += 1
