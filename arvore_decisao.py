4acd38ccee199034ea919a04f61dee21fbc46e84
Natalia <naty-aragao@hotmail.com>
2023-10-30 12:08:34 -0300

    finalizando a arvore com o gini

--- arvore_decisao.py
+++ arvore_decisao.py
@@ -14,6 +14,7 @@ alcohol = 'Alcohol'
 malic_acid = 'Malic Acid'
 hue = 'Hue'
 wine = 'Wine'
+previsores = [alcohol, malic_acid, hue, wine]
 
 df = data[[alcohol, malic_acid, hue, wine]]
 X = df[[alcohol, malic_acid, hue]]
@@ -22,11 +23,25 @@ y = filter.formatar_em_string(y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-arvore_decisao = DecisionTreeClassifier(criterion='entropy')
-arvore_decisao.fit(X_test, y_test)
-
-print(arvore_decisao.feature_importances_)
-previsores = [alcohol, malic_acid, hue, wine]
+arvore_decisao_entropia = DecisionTreeClassifier(criterion='entropy')
+arvore_decisao_entropia.fit(X_test, y_test)
+print("Entropia: ", arvore_decisao_entropia.feature_importances_)
+previsoes_entropia = arvore_decisao_entropia.predict([[13.39, 1.77, 0.92], [13.62, 4.95, 0.91]])
+print("Previsões Entropia: ", previsoes_entropia)
 figura, eixos = plt.subplots(nrows=1, ncols=1, figsize=(10,10))
-tree.plot_tree(arvore_decisao, feature_names=previsores, class_names=arvore_decisao.classes_, filled=True)
+tree.plot_tree(arvore_decisao_entropia, feature_names=previsores, class_names=arvore_decisao_entropia.classes_, filled=True)
 plt.show()
+
+
+arvore_decisao_gini = DecisionTreeClassifier(criterion='gini')
+arvore_decisao_gini.fit(X_test, y_test)
+print("Gini: ", arvore_decisao_gini.feature_importances_)
+previsoes_gini = arvore_decisao_entropia.predict([[13.39, 1.77, 0.92], [13.62, 4.95, 0.91]])
+print("Previsões Gini: ", previsoes_gini)
+
+figura, eixos = plt.subplots(nrows=1, ncols=1, figsize=(10,10))
+tree.plot_tree(arvore_decisao_gini, feature_names=previsores, class_names=arvore_decisao_gini.classes_, filled=True)
+plt.show()
+
+
+
